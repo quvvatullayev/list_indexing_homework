1c3fdec5f155be0d9351012f70fa46d3b7780614
tajriba <quvvatullayev@gmail.com>
2022-07-04 09:01:59 +0300

    List10.py

--- List10.py
+++ List10.py
@@ -6,4 +6,12 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return list_num[0]
+    number1 = list_num[0]
+    number2 = list_num[-1]
+    max = 0
+    if number1 > number2:
+        max += number1
+    else:
+        max += number2
+
+    return max
